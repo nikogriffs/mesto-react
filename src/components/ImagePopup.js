df5ceb5ac8b22d89dcd768f7218405bf2eca1f22
Nikolay Komarov <nikogriffs@yandex.ru>
2021-07-09 13:22:11 +0700

    исправил начальный тип данных для selectedCard

--- src/components/ImagePopup.js
+++ src/components/ImagePopup.js
@@ -6,8 +6,8 @@ function ImagePopup(props) {
             <div className="popup__container popup__container_card">
                 <button type="button" className="popup__close-button" onClick={props.onClose} />
                 <figure className="popup__figure">
-                    <img src={props.card.link} alt={props.card.name} className="popup__image" />
-                    <figcaption className="popup__caption">{props.card.name}</figcaption>
+                    <img src={props.card?.link} alt={props.card?.name} className="popup__image" />
+                    <figcaption className="popup__caption">{props.card?.name}</figcaption>
                 </figure>
             </div>
         </div>
