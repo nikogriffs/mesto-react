5712ec1514221f8b48e76c4073c862155732f8ca
Nikolay Komarov <nikogriffs@yandex.ru>
2021-07-12 03:37:24 +0700

    проверка

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,7 @@ import EditProfilePopup from './EditProfilePopup.js';
 import AddPlacePopup from './AddPlacePopup.js';
 import EditAvatarPopup from './EditAvatarPopup.js';
 import ImagePopup from './ImagePopup.js';
-import api from '../utils/api.js';
+import api from '../utils/apii.js';
 import { CurrentUserContext } from '../contexts/CurrentUserContext.js';
 
 function App() {
