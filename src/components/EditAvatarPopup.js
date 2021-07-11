504334e46edde140502e6bdb33e267363210b5a7
Nikolay Komarov <nikogriffs@yandex.ru>
2021-07-12 03:32:22 +0700

    fix2 автотесты

--- src/components/EditAvatarPopup.js
+++ src/components/EditAvatarPopup.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PopupWithForm from './PopupWithForm.js';
 
-function AvatarPopup(props) {
+function EditAvatarPopup(props) {
 
     const avatarRef = React.useRef();
 
